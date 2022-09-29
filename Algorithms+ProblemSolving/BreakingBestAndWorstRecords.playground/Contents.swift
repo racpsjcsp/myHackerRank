72c0cd249b498e700d7957de3caf175e3b6cf43b
Rafael Plinio <rafael.plinio@foursys.com.br>
2022-09-29 20:53:26 -0300

    Breaking Best And Worst Records

--- Algorithms+ProblemSolving/BreakingBestAndWorstRecords.playground/Contents.swift
+++ Algorithms+ProblemSolving/BreakingBestAndWorstRecords.playground/Contents.swift
@@ -1,3 +1,26 @@
 import Cocoa
+/*
+ https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+ */
 
-var greeting = "Hello, playground"
+func breakingRecords(scores: [Int]) -> [Int] {
+    var newMinRecordCount = 0
+    var newMaxRecordCount = 0
+    var minScore = scores[0]
+    var maxScore = scores[0]
+    
+    for score in scores {
+        if score < minScore {
+            minScore = score
+            newMinRecordCount += 1
+        }
+        
+        if score > maxScore {
+            maxScore = score
+            newMaxRecordCount += 1
+        }
+    }
+    return [newMaxRecordCount, newMinRecordCount]
+}
+
+breakingRecords(scores: [10, 5, 20, 20, 4, 5, 2, 25, 1])
